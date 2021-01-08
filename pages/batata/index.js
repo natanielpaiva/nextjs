9a8d969a5b3a397647cecebcf71149ca6861f0f4
Nataniel Paiva <nataniel.paiva@gmail.com>
2021-01-07 23:54:06 -0300

    Subnid

--- pages/batata/index.js
+++ pages/batata/index.js
@@ -1,13 +1,9 @@
 import Link from 'next/link'
 
-
-export default function FirstPost() {
-  return (
-    <h1 className="title">
-      Read{' '}
-      <Link href="/">
-        <a>this page!</a>
-      </Link>
-    </h1>
-  )
+export default function Batata(){
+    return (
+        <div>
+            <Link href='/'> Ir para a home </Link>
+        </div>
+    )
 }
